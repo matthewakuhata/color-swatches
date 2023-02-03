529056d6478b511cb237da301375f3ddad26b0d3
Matthew Akuhata <matthewakuhata@gmail.com>
2023-02-03 22:41:01 +1100

    Update tyeps

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,11 +1,13 @@
 export enum ColorType {
-   RGB = 'rgb',
-   HSL = 'hsl'
+  RGB = "rgb",
+  HSL = "hsl",
 }
 
 export type ColorTypeKey = keyof typeof ColorType;
 
 export type ColorSpaces = RGB | HSL;
+
+export type ColorTone = "light" | "dark";
 
 export type Colors = RGBColor | HSLColor;
 
@@ -14,21 +16,20 @@ export type RGBColor = Color & RGB;
 export type HSLColor = Color & HSL;
 
 export interface Color {
-   type: ColorType;
-   colorString: string;
-   hex: string;
-   tone: 'light' | 'dark';
+  type: ColorType;
+  colorString: string;
+  hex: string;
+  tone: ColorTone;
 }
 
-
 export interface RGB {
-   red: number;
-   blue: number;
-   green: number;
+  red: number;
+  blue: number;
+  green: number;
 }
 
 export interface HSL {
-   hue: number;
-   saturation: number;
-   lightness: number;
+  hue: number;
+  saturation: number;
+  lightness: number;
 }
